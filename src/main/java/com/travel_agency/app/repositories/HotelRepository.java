5f151c089b87a1b9743905cae1008278ba0f1e19
Pacorb94 <a@a.com>
2022-02-07 18:13:19 +0100

    ADD HotelRepository

---
+++ src/main/java/com/travel_agency/app/repositories/HotelRepository.java
@@ -0,0 +1,25 @@
+package com.travel_agency.app.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.travel_agency.app.models.Hotel;
+
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Long>{
+	
+	// Al usar findBy... ya está configurado para buscar
+	@Transactional(readOnly = true)
+	public Optional<List<Hotel>> findByCity(String city);
+
+	@Transactional(readOnly = true)
+	public Optional<List<Hotel>> findByPlacesAvailable(int placesAvailable);
+
+	@Transactional(readOnly = true)
+	public Optional<List<Hotel>> findByCategory(int category);
+}
